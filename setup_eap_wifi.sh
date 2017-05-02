dc52195602e0070fd4455291d5ec0d5968e114d6
Brian Hopkins <brianhh1230@gmail.com>
2017-05-02 10:11:11 -0400

    Update setup_eap_wifi.sh
    cleaning up formatting

--- setup_eap_wifi.sh
+++ setup_eap_wifi.sh
@@ -1,17 +1,23 @@
 #!/usr/bin/env bash
 # This script sets up WPA 2 Enterprise with PEAP by asking for your user id and pw and then hashes the pw and updates the wpa_supplicant file.
 # Author - btotharye
+
+
 # WPA Supplicant File Location
 CONFIG_FILE=/etc/wpa_supplicant/wpa_supplicant.conf
+
 #Gather Name of Wireless SSID
 echo What is your Wireless SSID you are trying to add?
 read ssid_network
+
 #Gather Userid for PEAP Wifi
 echo What is your userid for your wifi?
 read username
+
 #Gather PW then hash it for file
 echo What is your password for your userid?
 read -s -p "Password: " password
+
 #Stripping the (stdin)= part of hash
 hash_pw=$(echo -n $password| iconv -t utf16le | openssl md4)
 hash_pw_updated=$(echo -e $hash_pw | sed -r 's/^.{9}//')
