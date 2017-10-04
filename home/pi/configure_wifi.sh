38da40b8121cc9dc6b7cc2ef2e83d2e22b15728c
Michael Nguyen <ppnguyen91@gmail.com>
2017-10-04 16:00:56 -0500

    Update configure_wifi.sh

--- home/pi/configure_wifi.sh
+++ home/pi/configure_wifi.sh
@@ -3,7 +3,7 @@ python messagebus_emit.py mycroft.wifi.start
 
 echo "====================================================================="
 echo "Wifi setup has begun.  Use your phone, tablet or laptop to connect to"
-echo "the network 'MYCROFT' using the password 'MYCROFT1'.  Once connected"
+echo "the network 'MYCROFT' using the password '12345678'.  Once connected"
 echo "browse to 'https://start.mycroft.ai', then follow the prompts to"
 echo "complete the setup."
 echo "====================================================================="
