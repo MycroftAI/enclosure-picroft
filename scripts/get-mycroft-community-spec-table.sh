d196e08c8b136807c73b8e3221eea7411d7de006
Fermulator <github@fermulator.fastmail.org>
2020-11-10 19:36:01 -0500

    add enhancements per community discussion

--- scripts/get-mycroft-community-spec-table.sh
+++ scripts/get-mycroft-community-spec-table.sh
@@ -24,11 +24,19 @@ CPU_TEMP="$((CPU/1000))'C"
 # Use 'vc' to determine GPU temp in celsius
 GPU_TEMP="$(/opt/vc/bin/vcgencmd measure_temp | awk -F= '{print $2}')"
 
+# Use 'vc' to determine if we're being throttled
+PI_THROTTLING="$(vcgencmd get_throttled | awk -F\= '{print $2}')"
+
 # Use 'w' and print the standard system load averages (1/5/15 minutes)
 LOAD_AVGS="$(w | grep load | awk -F'load average:' '{print $2}')"
 
 # Print the CSV rST format for table
 echo
-echo "   \"${DATE}\", \"${PI_VERSION}\", \"${MYCROFT_CORE_VERSION}\", \"${PICROFT_VERSION}\", \"<UPDATE>\", \"<UPDATE>\", \"${CPU_TEMP}\", \"${GPU_TEMP}\", \"${LOAD_AVGS}\", \"<USER>\""
+echo "   DATE, PI VERSION, MYCROFT CORE VERSION, PICROFT VERSION, PI HW THERMAL MANAGEMENT, PI HW CASE, CPU TEMP, GPU TEMP, PI THROTTLING, LOAD AVGS, USER"
+echo "   \"${DATE}\", \"${PI_VERSION}\", \"${MYCROFT_CORE_VERSION}\", \"${PICROFT_VERSION}\", \"<UPDATE_PI_HW>\", \"<UPDATE_PI_CASE>\", \"${CPU_TEMP}\", \"${GPU_TEMP}\", \"${PI_THROTTLING}\", \"${LOAD_AVGS}\", \"<UPDATE_USER>\""
 echo
 
+# Print bonus info about culprit processor
+echo
+ps aux | head -1; ps haux | sort -nrk 3,3 | head -n 10
+echo
